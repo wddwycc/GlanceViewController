501edfe4319d1a8972ab0af5f0e2bd22f197313f
闻端Carl <wddwyss@gmail.com>
2016-04-05 21:33:55 +0800

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+GlanceViewController
+---
+Still Processing
---
+++ Sources/GalanceViewController.swift
@@ -0,0 +1,25 @@
+//
+//  GalanceViewController.swift
+//  Demo
+//
+//  Created by 闻端 on 16/4/5.
+//  Copyright © 2016年 monk-studio. All rights reserved.
+//
+
+import UIKit
+
+class GalanceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
